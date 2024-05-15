c60d798a50c35cf18b81696362afdca627b2e716
rseangueco <rngueco2@up.edu.ph>
2024-05-15 11:27:44 +0800

    feat: add routes for user-controller

--- src/databse-api/router.js
+++ src/databse-api/router.js
@@ -1,4 +1,5 @@
 import { getProducts, getProductById, addProduct, deleteProduct } from './product-controller.js';
+import { getUsers, getUserByEmail, addUser, deleteUser } from './user-controller.js';
 
 export default function router(app) {
  	// Allow Cross Origin Resource Sharing
@@ -10,8 +11,15 @@ export default function router(app) {
     next();
   })
   
+  // routes for product functions
   app.get('/get-products', getProducts);
   app.get('/get-product-by-id', getProductById);
   app.post('/add-product', addProduct);
   app.post('/delete-product', deleteProduct);
+
+  // routes for user functions
+  app.get('/get-users', getUsers);
+  app.get('/get-user-by-email', getUserByEmail);
+  app.post('/add-user', addUser);
+  app.post('/delete-user', deleteUser);
 }
