dd6a3b53caffe0c03f7a16f7d77a9212fc241db2
rseangueco <rngueco2@up.edu.ph>
2024-05-15 10:46:21 +0800

    feat: initialize database-api files'

---
+++ src/databse-api/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import router from './router.js';
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: dalse}));
+
+router(app)
+
+app.listen(3001, () => {console.log("API listening at port 3001.")});
