37f4c58d4320886ca629b05a0b7d241aa3732a72
zachglindro <zmglindro2@up.edu.ph>
2024-05-20 22:28:26 +0800

    fix: revise order transaction model

--- database-api/models/order-transaction-model.js
+++ database-api/models/order-transaction-model.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-const orderTransactionSchema = mongoose.Schema('Order_transaction', {
+const orderTransactionSchema = mongoose.Schema({
     transactionId: {type: String, required: true},
     productId: {type: String, required: true},
     orderQty: {type: Number, required: true},
     orderStatus: {type: Number, required: true},
     email: {type: String, required: true},
     dateOrdered: {type: Date, required: true},
-    time: {type: TimeRanges, required: true},
+    time: {type: Date, required: true},
 })
 
 const OrderTransaction = mongoose.model('Order_transaction', orderTransactionSchema)
