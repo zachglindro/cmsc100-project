c6bff74027183460828a725e5a68b4cfbc4f3536
russsss-19 <jtilagan3@up.edu.ph>
2024-05-22 12:30:17 +0800

    feat: add methods to merchant

--- database-api/models/order-transaction-model.js
+++ database-api/models/order-transaction-model.js
@@ -4,10 +4,10 @@ const orderTransactionSchema = mongoose.Schema({
     transactionId: {type: String, required: true},
     productId: {type: String, required: true},
     orderQty: {type: Number, required: true},
-    orderStatus: {type: Number, required: true},
+    orderStatus: {type: Number, enum: [1,2,3], required: true},
     email: {type: String, required: true},
     dateOrdered: {type: Date, required: true},
-    time: {type: Date, required: true},
+    time: {type: TimeRanges, required: true},
 })
 
 const OrderTransaction = mongoose.model('Order_transaction', orderTransactionSchema)
--- database-api/routes/merchant.js
+++ database-api/routes/merchant.js
@@ -9,4 +9,31 @@ const getOrders = async (req, res) => {
   }
 };
 
+const createOrder = async (req,res) => {
+  try{
+    const {transactionId, productId, orderQty, email, dateOrdered, time} = req.body
+    
+    const newOrder = new OrderTransaction({transactionId, productId, orderQty, email, dateOrdered, time, orderStatus: 1})
+    
+    res.status(201).json({message: "Order transaction successfully created"});
+  } catch (error) {
+    res.status(500).json({error: "Unable to create order transaction"})
+  }
+}
+
+const confirmOrder = async (req,res) => {
+  try {
+    transactionId = req.body.transactionId
+
+    await db.collection('Order-transactions').updateOne(
+      {transactionId: transactionId},
+      { $set: {orderStatus: 2}}
+    )
+
+    res.status(201).json({message: "Order successfully confirmed"})
+  } catch (error) {
+
+  }
+}
+
 export { getOrders };
