0bd1b81948f456d2b989e7f1483a670b097fdb98
PAN5Y <apgalido@up.edu.ph>
2024-05-28 11:09:04 +0800

    feat: email should be in right format; sorted the orders in customers page (pending being first)

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -52,19 +52,19 @@ function SignUp() {
         <form className='sign-up-form' onSubmit={handleRegister}>
           <br />
           <div className="sign-up"> SIGN UP </div>
-          <input className='input' type='text' placeholder='First Name' value={fname} onChange={(e) => setFname(e.target.value)} />
+          <input className='input' type='text' placeholder='First Name' value={fname} onChange={(e) => setFname(e.target.value)} required/>
           {/* <br /> */}
           <br />
-          <input className='input' type='text' placeholder='Last Name' value={lname} onChange={(e) => setLname(e.target.value)} />
+          <input className='input' type='text' placeholder='Last Name' value={lname} onChange={(e) => setLname(e.target.value)} required/>
           {/* <br /> */}
           <br />
-          <input className='input' type='text' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input className='input' type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
           {/* <br /> */}
           <br />
-          <input className='input' type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input className='input' type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} required/>
           {/* <br /> */}
           <br />
-          <input className='input' type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input className='input' type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
           <br />
           {/* <br /> */}
           <div className="bottom-container"> 
